e0787cb84c5a37d2d88e5bb1176778e1f448fd9b
Prabin1025y <acharyaprabin1025y@gmail.com>
2026-02-10 19:54:29 +0545

    feat: add health check endpoint and dotenv configuration for environment variables

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import { shutdown } from './utils/shutDown.js';
 import router from './router/badge.router.js';
+import https from 'https';
+// import http from 'http';
+import "dotenv/config";
 
 const app = express();
 app.use(express.static('public')); // Serve static files from the "public" directory
 const PORT = process.env.PORT || 3000;
 
-app.use('/badge.svg',router);
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
+app.use('/badge.svg', router);
 
 // Graceful shutdown
 process.on('SIGTERM', shutdown);
@@ -16,3 +23,22 @@ app.listen(PORT, () => {
     console.log(`🚀 GitHub Contribution Scraper running on port ${PORT}`);
     console.log(`📊 Badge: http://localhost:${PORT}/badge.svg?username=yourname`);
 });
+
+const INTERVAL_DELAY = 8 * 60 * 1000; // 8mins
+const url = new URL(`${process.env.API_URL}/health`);
+
+// don't sleep
+setInterval(() => {
+    https
+        .get(url.href)
+        .on("response", () => {
+            console.log(`api HEALTH_CHECK successful; ${url.href}`);
+        })
+        .on("error", (err) =>{
+            console.warn(
+                `api HEALTH_CHECK failed; ${err.message.trim()}`
+            )
+            // console.log(err)
+        }
+        );
+}, INTERVAL_DELAY);
